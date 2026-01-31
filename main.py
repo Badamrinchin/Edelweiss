8198676c1ea5265c4d6e698e09e09cdc1f0f5eed
Badamrinchin <badamrinchin@gmail.com>
2026-02-01 00:34:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,49 +7,42 @@ app = FastAPI()
 
 SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzlWULv-JbOHbHpiiOFnb39Hw_8uRZusR5-Rm1GapuCjRq_I1NSZ3eMfxCPlPXkA3ollQ/exec"
 
-index_html = """
-<!DOCTYPE html>
-<html lang="mn">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Edelweiss Yoga Center – Бүртгэл</title>
-</head>
-<body>
-    <h2>Edelweiss Yoga Center – Бүртгэл</h2>
-    <form method="post" action="/register">
-        <label>Багш</label><br>
-        <select name="teacher">
-            <option>Өлзийжаргал</option>
-            <option>Өлзийдэлгэр</option>
-            <option>Өлзийбаяр</option>
-            <option>Тунгалаг</option>
-        </select><br><br>
-
-        <label>Өдөр</label><br>
-        <input name="schedule"><br><br>
-
-        <label>Цаг</label><br>
-        <input name="time"><br><br>
-
-        <label>Нэр</label><br>
-        <input name="client"><br><br>
-
-        <label>Утас</label><br>
-        <input name="phone"><br><br>
-
-        <label>Төлбөр</label><br>
-        <input name="price" type="number"><br><br>
-
-        <button type="submit">Бүртгэх</button>
-    </form>
-</body>
-</html>
-"""
 
 @app.get("/", response_class=HTMLResponse)
 def index():
-    return index_html
+    return """
+    <html>
+      <head>
+        <title>Edelweiss Yoga Center – Бүртгэл</title>
+      </head>
+      <body>
+        <h2>Edelweiss Yoga Center – Бүртгэл</h2>
+
+        <form method="post" action="/register">
+          <p>Багш</p>
+          <input name="teacher" required>
+
+          <p>Өдөр</p>
+          <input name="schedule" required>
+
+          <p>Цаг</p>
+          <input name="time" required>
+
+          <p>Үйлчлүүлэгчийн нэр</p>
+          <input name="client" required>
+
+          <p>Утас (8 оронтой)</p>
+          <input name="phone" required>
+
+          <p>Төлбөр</p>
+          <input name="price" type="number" required>
+
+          <br><br>
+          <button type="submit">Бүртгэх</button>
+        </form>
+      </body>
+    </html>
+    """
 
 
 @app.post("/register", response_class=HTMLResponse)
@@ -61,8 +54,8 @@ def register(
     phone: str = Form(...),
     price: int = Form(...)
 ):
-    if not re.fullmatch(r"\d{8}$", phone):
-        return "❌ Утас буруу <a href='/'>Буцах</a>"
+    if not re.fullmatch(r"\d{8}", phone):
+        return "❌ Утасны дугаар буруу <a href='/'>Буцах</a>"
 
     payload = {
         "teacher": teacher,
